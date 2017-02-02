7f6386d73572cc0a98f882f82b0c7069bca87d94
Eiji Masaki <eijimasaki@beyer.local>
2017-02-03 00:23:50 +0900

    クライアントからサーバへのメッセージの送信ができるようになった。

--- chatclient/chatclient.go
+++ chatclient/chatclient.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
 func main() {
+	conn, err := net.Dial("tcp", "localhost:1234")
+	if err != nil {
+		fmt.Printf("connection error: %s\n", err)
+		return
+	}
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		t := scanner.Text()
+		if t == "exit" {
+			break
+		}
+		conn.Write([]byte(t))
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
